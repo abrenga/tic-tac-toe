874062c23c8d3327078294184862c1a7ba5ee150
Netta90 <58398245+Netta90@users.noreply.github.com>
2021-03-01 12:13:26 +0100

    nuovo commit

--- js/index.js
+++ js/index.js
@@ -15,9 +15,31 @@ $(document).ready(function () {
 });
 
 
-let tris ={
-
-
+let trisArray = [];
 
     
+
+let boxOne = document.getElementById("box-one");
+let boxTwo = document.getElementById("box-two");
+let boxThree = document.getElementById("box-third");
+let boxFour = document.getElementById("box-four");
+let boxFive = document.getElementById("box-five");
+let boxSix = document.getElementById("box-six");
+let boxSeven = document.getElementById("box-seven");
+let boxEight = document.getElementById("box-eight");
+let boxNine = document.getElementById("box-nine");
+
+let tris = {
+
+    
+
+     puhareNellìarray: function(box){
+        trisArray.push(box);
+
+
+    }
+
+
+
+
 }
