8c929d49d71abd5304953c6b5e965a559a7afcf2
Netta90 <antobrg90@hotmail.it>
2021-03-03 22:26:48 +0100

    uff

--- js/index.js
+++ js/index.js
@@ -1,17 +1,35 @@
-tris = {
+let arrayDicelle = [];
 
-    creaCella = function () {
+
+let tris = {
+
+    creaCella: function () {
         let box = {
+            x: 0,
+            y: 0,
             content: ""
         }
         return box;
     },
 
-    generaCelle: function (  ) {
-        for (let i = 1; i <= 9; i++) {
-            let cella = this.creaCella();
+    generaCelle: function (sizeX, sizeY) {
+        for (let x = 0; x < sizeX; x++) {
+            for (let y = 0; y < sizeY; y++) {
+                let cella = this.creaCella();
+                cella.x = x;
+                cella.y = y;
+                arrayDicelle.push(cella);
+            }
+        }
+    },
+
+    selezionaCasella: function (x, y) {
+        arrayDicelle.forEach(box => {
+            if (box.x == x && box.y == y){
+                return box;
+            }
             
-        } 
+        });
 
     },
 
@@ -19,6 +37,8 @@ tris = {
     insericiLaX: function () {
 
     },
+
+
     insericiLaO: function () {
 
     },
@@ -30,4 +50,3 @@ tris = {
     }
 
 }
-
