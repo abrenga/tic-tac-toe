b1c5d5e683ce08aed56e78f4abaab82c5aedbc38
Netta90 <58398245+Netta90@users.noreply.github.com>
2021-03-04 18:48:28 +0100

    meglio di ieri

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 let arrayDicelle = [];
 
 
-let tris = {
+/*let tris = {
 
     creaCella: function () {
         let box = {
@@ -12,7 +12,18 @@ let tris = {
         return box;
     },
 
-    generaCelle: function (sizeX, sizeY) {
+
+    per generare la griglia essendo questa fatta di 2 misure ovvero una lunghezza x e una lunghezza y
+     io assegnerò alla funzione due parametri che fungono da lunghezza definitiva della griglia(le lunghezze sono due).
+     arbitrariamente assegnamo sizeX e sizeY.
+
+     dato che le lunghezze sono due e non una per incrociare le coordinate bisogna fare due for uno dentro l'altro, cosi nel primo for andiamo a scorrerte tutta la lunghezza x e per ogni elemento della lunghezza x corrispondera ogni(tutti) elemento y.
+     Ogni giro del for avrà un parametro x e un parametro y quando ha finito per il primo parametro x tutti i parametri y ripartira con il secondo parametro X.
+
+Bene! Ora che li abbiamo scorsi dobbiamo puntarli ovvero assegnamo ai parametri box.X e box.y esattamente la x(che sarà ogni x che scorre assume su ogni for) e la y(uguale corrispondente).
+Abbiamo cosi ogni box con le sue coordinate esatte. pushamo poi tutto nell'arrey (quindi l'intera cella).
+
+    generaCelle: function (sizeX, sizeY) {  
         for (let x = 0; x < sizeX; x++) {
             for (let y = 0; y < sizeY; y++) {
                 let cella = this.creaCella();
@@ -22,6 +33,15 @@ let tris = {
             }
         }
     },
+
+
+    Ora arrivati a questo punto abbiamo ogni box con le coordiante esatte. ogniuno con le sue.
+    Dobbiamo rendere posibile la selezione come?
+    tieni presente ora che ogni box tu l'hai messo in un'array, quindi possiamo scorrerlo tranquillamente per ogni elemento dell'arrey
+    e per ciascuno di questi mi chiedo se box.x e box.y sono uguali alla coordinata che sto selezionando in quel moemnto(perchè ricordati che hai assegnato in automatico a tutti i box delle coordinate specifiche)
+    se si ritornami esattamente quel box che ho cliccato e solo quello.
+
+
 
     selezionaCasella: function (x, y) {
         arrayDicelle.forEach(box => {
@@ -48,5 +68,81 @@ let tris = {
     inizioGioco: function () {
 
     }
+
+}*/
+
+let arrayCelle = [];
+
+let toccaX = true;
+
+
+let tris = {
+
+    creaCella: function () {
+        let box = {
+            X: "0",
+            Y: "0",
+            content: " "
+
+        },
+        return box;
+
+    },
+
+    creaGriglia: function (sizeX, sizeY) {
+        for (let x = 0; x <= sizeX; x++) {
+            for (let y = 0; y <= sizeY; y++) {
+
+                let cella = this.creaCella();
+                box.X = x;
+                box.Y = y;
+                arrayCelle.push(cella);
+            }
+        }
+
+    },
+
+
+    selezionaCella: function (x, y) {
+        arrayCelle.forEach(box => {
+            if (box.X == x && box.Y == y) {
+
+                return box
+
+            }
+
+        })
+
+    },
+
+
+    inserisciX: function () {
+        let cella = this.selezionaCella(x, y)
+
+        if (cella.content == "" && toccaX == true) {
+
+            cella.content = "X";
+        }
+        toccaX = false;
+    },
+
+
+    inserisciY: function () {
+        let cella = this.selezionaCella(x, y)
+
+        if (cella.content == "" && toccaX !== true) {
+
+            cella.content = "Y";
+        }
+
+        toccaX = true;
+
+    },
+
+
+
+
+
+
 
 }
