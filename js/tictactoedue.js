c5310a82e7dd651af03838ddf275c9ee3fb87e7e
Netta90 <58398245+Netta90@users.noreply.github.com>
2021-10-18 12:29:17 +0200

    nuovo esercizio tic tac

---
+++ js/tictactoedue.js
@@ -0,0 +1,15 @@
+
+function inserisciLaX(){
+    
+}
+
+function inserisciLaO (){
+
+}
+
+
+btns.forEach(btn => {
+    btn.addEventListener("click", function(){
+        if(btn.value == ""  btn.value== "X")
+    })
+});
