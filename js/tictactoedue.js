2ea07c0e8c191d67c1fd8e22aa062e84c7c40cb2
Netta90 <58398245+Netta90@users.noreply.github.com>
2021-10-19 12:29:16 +0200

    nuovo

--- js/tictactoedue.js
+++ js/tictactoedue.js
@@ -2,37 +2,17 @@ let celle = [];
 let toccaAllaX = true;
 
 
-<<<<<<< HEAD
 function creaCella(x, y) {
-    let box = {
-        cella: document.getElementById("box_" + x + "-" + y),
-        posizioneX: x,
-        posizioneY: y
-    }
-
-    celle.push(box);
-
-=======
-function associaCella(x, y) {
-    let cella = document.getElementById('box_' + x + '-' + y);
-    console.log(cella)
-    return cella
-    
->>>>>>> b518d47693adcfd01fef47bb52606f04e6da1d58
+    let cella = document.getElementById("box_" + x + "-" + y)
+    celle.push(cella);
 }
 
 
 
 function creaGriglia(sizeX, sizeY) {
-<<<<<<< HEAD
     for (x = 0; x < sizeX; x++) {
         for (y = 0; y < sizeY; y++) {
             creaCella(x, y);
-=======
-    for (y = 0; y <= sizeY; y++) {
-        for (x = 0; x <= sizeX; x++) {
-            associaCella(x, y);
->>>>>>> b518d47693adcfd01fef47bb52606f04e6da1d58
         }
     }
 }
@@ -40,16 +20,18 @@ function creaGriglia(sizeX, sizeY) {
 creaGriglia(3, 3);
 
 
-console.log(celle)
 celle.forEach(cella => {
-       cella.cella.addEventListener("click", function(){
-           console.log(cella.cella)
-       })
+    cella.addEventListener("click", function () {
+        if (cella.textContent == "" && toccaAllaX == true) {
+            inserisciLaX(cella);
 
+
+        } else {
+            inserisciLaO(cella)
+        }
+    })
 
 });
-
-
 
 
 creaGriglia(3, 3);
@@ -67,27 +49,6 @@ function inserisciLaO(btn) {
 
 }
 
-
-/*btns.forEach(btn => {
-    btn.addEventListener("click", function () {
-<<<<<<< HEAD
-        if (btn.textContent == "" && toccaAllaX == true) {
-            let cella= selezionaCella(btn.x,btn.y)
-            inserisciLaX(cella);
-            console.log(cella)
-
-=======
-        console.log(btn)
-        if (btn.textContent == "" && toccaAllaX == true) {
-            inserisciLaX(btn);
->>>>>>> b518d47693adcfd01fef47bb52606f04e6da1d58
-        } else {
-            inserisciLaO(btn)
-        }
-
-    }
-    )
-});*/
 
 
 
